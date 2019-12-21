bb482da7b0d617cbf09e18663e183878e176b560
Sansossio <juliosansossio@gmail.com>
2019-12-21 19:06:26 +0100

    Load matches sequential

--- src/main/kotlin/com/twisted/lolmatches_loader/match/MatchService.kt
+++ src/main/kotlin/com/twisted/lolmatches_loader/match/MatchService.kt
@@ -1,16 +1,11 @@
 package com.twisted.lolmatches_loader.match
 
 import com.twisted.dto.match_loading.MatchLoadingMatches
-import com.twisted.lolmatches_loader.entity.match.MatchDocument
 import com.twisted.lolmatches_loader.entity.match.MatchRepository
 import com.twisted.lolmatches_loader.entity.match_loading.MatchLoadingDocument
 import com.twisted.lolmatches_loader.entity.match_loading.MatchLoadingRepository
 import com.twisted.lolmatches_loader.mapper.match.matchToDocument
 import com.twisted.lolmatches_loader.riot.RiotService
-import net.rithms.riot.api.endpoints.match.dto.Match
-import net.rithms.riot.api.endpoints.match.dto.MatchTimeline
-import net.rithms.riot.api.request.AsyncRequest
-import net.rithms.riot.api.request.RequestAdapter
 import net.rithms.riot.constant.Platform
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
@@ -22,17 +17,18 @@ class MatchService(
         private val repository: MatchRepository,
         private val loadingRepository: MatchLoadingRepository
 ) {
+  private val api = riotApi.getApi()
+
   // Internal methods
-  private fun setLoadedMatch(match: MatchDocument) {
-    val matchesLoading = loadingRepository.findMatch(match.game_id, match.region)
+  private fun setLoadedMatch(game_id: Long, region: Platform) {
+    val matchesLoading = loadingRepository.findMatch(game_id, region.toString())
     for (matchLoading in matchesLoading) {
       for (oneMatch in matchLoading.matches) {
-        if (match.game_id == oneMatch.game_id) {
+        if (game_id == oneMatch.game_id) {
           oneMatch.loading = false
         }
       }
       loadingRepository.save(matchLoading)
-      repository.save(match)
     }
   }
 
@@ -43,69 +39,40 @@ class MatchService(
 
   private fun filterMatches(matchLoading: MatchLoadingDocument): List<MatchLoadingMatches> = matchLoading.matches.filter { m -> m.loading }
 
-  // External api methods
-  private fun getMatchesTimeline(matchList: List<Match>, region: Platform): Map<Long, MatchTimeline> {
-    val response = mutableMapOf<Long, MatchTimeline>()
-    val asyncApi = riotApi.getAsynApi()
-    for (match in matchList) {
-      asyncApi.getTimelineByMatchId(region, match.gameId).also {
-        it.addListeners(object : RequestAdapter() {
-          override fun onRequestSucceeded(request: AsyncRequest) {
-            response[match.gameId] = request.getDto<MatchTimeline>()
-          }
-        })
-      }
-    }
-    asyncApi.awaitAll()
-    return response
-  }
-
-  private fun getAllMatchesDetails(matchList: List<MatchLoadingMatches>, region: Platform): List<Match> {
-    val asyncApi = riotApi.getAsynApi()
-    val allMatchDetails = mutableListOf<Match>()
-    val matchListFiltered = matchList.filter { m ->
-      !existsByGameIdAndRegion(m.game_id, region)
-    }
-    for (match in matchListFiltered) {
-      asyncApi.getMatch(region, match.game_id).also {
-        it.addListeners(object : RequestAdapter() {
-          override fun onRequestSucceeded(request: AsyncRequest) {
-            allMatchDetails.add(request.getDto<Match>())
-          }
-        })
-      }
-    }
-    asyncApi.awaitAll()
-    return allMatchDetails
-  }
-
-  private fun loadAllMatches(matches: List<Match>, region: Platform): Int {
-    val matchesTimeline = getMatchesTimeline(matches, region)
-    for (match in matches) {
-      val timeline = matchesTimeline[match.gameId] ?: continue
-      val document = matchToDocument(
-              match = match,
-              matchTimeline = timeline
-      )
-      setLoadedMatch(document)
-    }
-    return matches.size
-  }
-
   private fun setMatchLoadingStatus(id: String, healthy: Boolean) {
     val instance = loadingRepository.findById(id).get()
     instance.healthy = healthy
     loadingRepository.save(instance)
   }
 
+  private fun processMatches(matchLoading: MatchLoadingDocument) {
+    val matches = filterMatches(matchLoading)
+    val region = riotApi.parseRegion(matchLoading.region)
+    for (match in matches) {
+      val gameId = match.game_id
+      val exists = existsByGameIdAndRegion(gameId, region)
+      if (!exists) {
+        val matchDetails = getMatchDetails(gameId, region)
+        val matchTimeline = getMatchTimeline(gameId, region)
+        repository.save(matchToDocument(
+                match = matchDetails,
+                matchTimeline = matchTimeline
+        ))
+      }
+      setLoadedMatch(gameId, region)
+    }
+  }
+
+  // External methods
+  private fun getMatchDetails(game_id: Long, region: Platform) = api.getMatch(region, game_id)
+
+  private fun getMatchTimeline(game_id: Long, region: Platform) = api.getTimelineByMatchId(region, game_id)
+
   // Public methods
   @Async
   fun loadMatches(matchLoading: MatchLoadingDocument) {
-    val region = riotApi.parseRegion(matchLoading.region)
-    val loadingMatches = filterMatches(matchLoading)
-    val matchesDetails = getAllMatchesDetails(loadingMatches, region)
     try {
-      loadAllMatches(matchesDetails, region)
+      processMatches(matchLoading)
       setMatchLoadingStatus(id = matchLoading.id, healthy = true)
     } catch (e: Exception) {
       setMatchLoadingStatus(id = matchLoading.id, healthy = false)
